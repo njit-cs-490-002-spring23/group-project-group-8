a46758a0647ee43599e80d32304990433a67dc81
Wes Andrade <39351248+malloc-wes@users.noreply.github.com>
2023-05-04 18:26:33 -0400

    add
    add useBox container for the car movement

--- frontend/src/components/Town/interactables/kartdash/src/Car.tsx
+++ frontend/src/components/Town/interactables/kartdash/src/Car.tsx
@@ -1,5 +1,6 @@
+import { useBox } from '@react-three/cannon';
 import { useLoader } from '@react-three/fiber';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Object3D } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
@@ -11,5 +12,37 @@ export function Car(): JSX.Element {
     (mesh.children[0] as Object3D).position.set(-365, -18, -67);
   }, [mesh]);
 
-  return <primitive object={mesh} rotation-y={Math.PI} />;
+  /**
+   * Dimensions of the player's car.
+   */
+  const carWidth = 0.1;
+  const carHeight = 0.05;
+  const carFront = 0.1;
+  const carPosition = [-1.5, 0.5, 3];
+  const carWheelRadius = 0.03;
+
+  /**
+   * Initializer for the car as a physics object using useBox hook from cannon.
+   */
+  const carChassisArguments: [number, number, number] = [carWidth, carHeight, carFront * 2];
+  const [carChassis, carChassisHook] = useBox(
+    () => ({
+      args: carChassisArguments,
+      mass: 150,
+      position: [0, 0, 0],
+    }),
+    useRef(null),
+  );
+
+  useEffect(() => {
+    mesh.scale.set(0.001, 0.0012, 0.0012);
+    (mesh.children[0] as Object3D).position.set(-365, -18, -67);
+  }, [mesh]);
+
+  return (
+    <mesh ref={carChassis}>
+      <meshBasicMaterial transparent={true} opacity={0.3} />
+      <boxGeometry args={carChassisArguments} />
+    </mesh>
+  );
 }
